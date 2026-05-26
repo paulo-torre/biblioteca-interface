f36449c88eb00766e4063c875cdd472f677213ec
Matheus Luiz <sergioabreu@MacBook-Pro-16-do-Matheus-Luiz.local>
2026-05-26 06:12:43 -0300

    chore: remove arquivos do login para isolar em outra branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/pages/register/register.component';
-import { LoginComponent } from './auth/pages/login/login.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
   { path: 'cadastro', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
 ];
